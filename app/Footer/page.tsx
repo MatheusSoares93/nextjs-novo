9fdd0f9f3c937b8f224826eb85fc27099e5776bc
MatheusSoares93 <142833871+MatheusSoares93@users.noreply.github.com>
2023-10-30 20:08:58 +0000

    First

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,8 +1,42 @@
 export default function Footer (){
-    return}
+    return(
 
     <>
-    <div className="">
+    <div className="flex bg-black w-full">
+    <article className="text-amber-500 w-1/4 pb-8">
+      <h1 className="pt-20 pl-10">Titulo</h1>
+      <br />
+      <hr />
+      <p className="text-white pt-4 pl-10 pr-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, maiores culpa sit voluptas fugiat est nesciunt magni nihil error, excepturi quasi reiciendis dolorum numquam cum voluptatum commodi porro itaque minima?</p>
+    </article>
+
+    <article className="w-1/4 pb-8">
+      <h1 className="pt-8 pl-28 text-white">Links</h1>
+      <ul className="pt-8 pl-4">
+        <li className="pt-4 pl-28 text-white">Link1</li>
+        <li className="pt-4 pl-28 text-white">Link2</li>
+        <li className="pt-4 pl-28 text-white">Link3</li>
+        <li className="pt-4 pl-28 text-white">Link4</li>
+        <li className="pt-4 pl-28 text-white">Link5</li>
+      </ul>
+    </article>
+
+    <article className="w-1/4 pb-8">
+      <h1 className="pt-8 pl-4 text-white">Links</h1>
+      <ul className="pt-8 pl-8">
+        <li className="pt-4 pl-0 text-white">Link1</li>
+        <li className="pt-4 pl-0 text-white">Link2</li>
+        <li className="pt-4 pl-0 text-white">Link3</li>
+        <li className="pt-4 pl-0 text-white">Link4</li>
+        <li className="pt-4 pl-0 text-white">Link5</li>
+      </ul>
+    </article>
+
+    <article className="w-1/4 pb-8 ">
+      <h1 className="pt-8 pl-4 bg-white w-52">Envie seu e-mail <button className="pt-8 pl-4 bg-amber-500">Enviar</button></h1>
+    </article>
 
     </div>
     </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Menu from './Menu/Page'
+import Footer from './Footer/page'
 
 export default function Home() {
   return (
     <>
     
       <Menu/>
+      <Footer/>
     </>
   )
 }
