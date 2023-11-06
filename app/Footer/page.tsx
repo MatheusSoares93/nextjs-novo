fcc088aa2e4641732744b6501e3e5d625bdc6554
MatheusSoares93 <142833871+MatheusSoares93@users.noreply.github.com>
2023-11-06 17:09:38 +0000

    first

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,42 +1,52 @@
-export default function Footer (){
-    return(
+export default function Footer(){
+  return(
+      <>
+      <div className="bg-black flex pt-8 justify-between pl-40 pr-40">
+<p className="text-amber-400">Still You Need Our Support ?</p>
+<form className="items-center">
+<input value=" Digite seu e-mail" className="bg-white text-sm rounded-l-xl p-1" /><button className="bg-amber-400 rounded-r-xl p-1">Enviar</button>
+</form>      
+      </div>
 
-    <>
-    <div className="flex bg-black w-full">
-    <article className="text-amber-500 w-1/4 pb-8 pl-20">
-      <h1 className="pt-20 pl-10">Titulo</h1>
-      <br />
-      <hr />
-      <p className="text-white pt-4 pl-10 pr-10">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, maiores culpa sit voluptas fugiat est nesciunt magni nihil error, excepturi quasi reiciendis dolorum numquam cum voluptatum commodi porro itaque minima?</p>
-    </article>
+      <div className=" text-white bg-black inline-flex justify-between pl-20 pr-20 pt-20 pb-5">
+      <article className="w-3/12 ">
+          <h1><a className="text-4xl text-amber-400">A</a>bout</h1>
+      <hr className="text-yellow-400" />  
+      <p className="w-full mt-2">Corporate clients and leisure travelers hasbeen relying on Groundlink for dependablesafe, and professional chauffeured carservice in major cities across World.</p>
+      <hr className="text-yellow-400" />
+      </article>
+      
+      <article className="w-3/12 pl-40">
+          <h1>Useful Links</h1>
+              <ul className="mt-1">
+                  <li>About</li>
+                  <li>News</li>
+                  <li>Patners</li>
+                  <li>Team</li>
+                  <li>Menu</li>
+                  <li>Contact</li>
 
-    <article className="w-1/4 pb-8">
-      <h1 className="pt-8 pl-28 text-white">Useful Links</h1>
-      <ul className="pt-8 pl-4">
-        <li className="pt-4 pl-28 text-white">Link1</li>
-        <li className="pt-4 pl-28 text-white">Link2</li>
-        <li className="pt-4 pl-28 text-white">Link3</li>
-        <li className="pt-4 pl-28 text-white">Link4</li>
-        <li className="pt-4 pl-28 text-white">Link5</li>
-      </ul>
-    </article>
+              </ul>
+      </article>
+      <article className="w-3/12 pl-20">
+          <h1>Help?</h1>
+          <ul className="mt-1">
+                  <li>FAQ</li>
+                  <li>Term & Condition</li>
+                  <li>Reporting</li>
+                  <li>Documentation</li>
+                  <li>Support Policy</li>
+                  <li>Privacy</li>
 
-    <article className="w-1/4 pb-8">
-      <h1 className="pt-8 pl-4 text-white">Links</h1>
-      <ul className="pt-8 pl-8">
-        <li className="pt-4 pl-0 text-white">Link1</li>
-        <li className="pt-4 pl-0 text-white">Link2</li>
-        <li className="pt-4 pl-0 text-white">Link3</li>
-        <li className="pt-4 pl-0 text-white">Link4</li>
-        <li className="pt-4 pl-0 text-white">Link5</li>
-      </ul>
-    </article>
+              </ul>
+      
+      </article>
+      <article className="w-3/12 text-yellow-500">
+          <h1>Recent Post</h1>
+      
+      </article>
+      </div>
 
-    <article className="w-1/4 pb-8 ">
-      <h1 className="pt-0 pl-4 bg-white w-52 h-10">Envie seu e-mail <button className="pt-0 pl-4 bg-amber-500">Enviar</button></h1>
-    </article>
-
-    </div>
-    </>
-    )
+      </>
+  )
 }
